578662b66062fe735f7880d098ed4eb9c9a500c8
Tristen-Hoffarth <hoffartt9643@student.faytechcc.edu>
2026-02-11 23:02:48 +0000

    Started M2T1

---
+++ M2T1.cpp
@@ -0,0 +1,40 @@
+// M2T1 - Product Sales
+// Tristen
+// 2/11/26
+
+#include <iostream>
+#include <iomanip>
+using namespace std;
+
+int main() {
+
+    //Set up variables
+    string first_name, last_name, full_name; //Hold customer name
+    string product = "apples";
+    double cost_each = 0.99;
+    double totale_cost;
+
+
+    //greet the customer
+    cout << "Welcome to our " << product << " store!" << endl;
+    cout << "What's your first name? ";
+    cin >> first_name;
+    cout << "What's your last name? ";
+    cin >> last_name;
+    full_name = first_name + " " + last_name;
+    cout << "Nice to meet you, " << full_name << endl;
+
+
+
+    //Ask how many they would like to purchase
+
+    //Calculate the total price
+    //Give the result
+
+
+
+
+
+
+    return 0; // no errors
+}
