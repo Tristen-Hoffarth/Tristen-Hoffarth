34b0580a3a25645d5cf6ec1d0a143a5817cc1ea7
Tristen-Hoffarth <hoffartt9643@student.faytechcc.edu>
2026-02-17 22:47:07 +0000

    M2T2 halfway done

---
+++ M2T2.cpp
@@ -0,0 +1,42 @@
+/*
+CSC-134
+M2T2 - Receipt Calculator
+Tristen
+2/17/26
+*/
+
+#include <iostream>
+#include <iomanip> //For decimals
+using namespace std;
+
+int main() 
+{
+    //Create a simple receipt
+    //Should include slaes tax
+
+    //Declare variables
+    string item = "🍝 Spaghetti";
+    double item_price = 5.99;
+    double tax_percent = 0.08; //8% is 8/100
+    double tax_amount;         //tax in $
+    double total;              //price * tax
+
+    //Greet the user
+    cout << "Welcome to our CSC 134 restaurant!" << endl;
+    cout << "You ordered one " << item << "." << endl;
+
+    //Calculate the meal price
+    //Calculate the sales tax and total
+    tax_amount = item_price * tax_percent; //take 8% of the item
+    total = item_price + tax_amount;
+
+
+
+    //Print the receipt
+    cout << setprecision(2) << fixed;
+    cout << total << endl;
+
+
+    return 0; //No errors
+
+}
